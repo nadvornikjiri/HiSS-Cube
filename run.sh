9ef960b8f7b6407ffba2c4a96a63e886a8c2dd9a
Jiri Nadvornik <nadvornik.ji@gmail.com>
2022-04-21 14:41:25 +0200

    Server config

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #bin/bash
-mpirun -np 128 --hostfile hosts --map-by node /gpfs/raid/SDSSCube/venv_par/bin/python hisscube.py --truncate ../sdss_data/ results/SDSS_cube_c_par.h5
+mpirun -np 33 --hostfile hosts --map-by node /gpfs/raid/SDSSCube/venv_par/bin/python hisscube.py --truncate ../sdss_data/ results/SDSS_cube_c_par.h5
