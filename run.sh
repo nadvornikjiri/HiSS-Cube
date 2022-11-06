b44750ebd1f7920c612992ff140012bebcf032fc
Jiri Nadvornik <nadvoji1@gmail.com>
2022-11-06 14:38:24 +0100

    Added parameter for number of cpus

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #bin/bash
 dir=$(pwd -P)
-mpirun -x DARSHAN_CONFIG_PATH=${dir}/darshan.conf -x LD_PRELOAD=${dir}/../shared_libs/darshan/darshan-runtime/lib/.libs/libdarshan.so:${dir}/ext_lib/hdf5-1.12.2/hdf5/lib/libhdf5.so -x LD_LIBRARY_PATH=${dir}/ext_lib/hdf5-1.12.2/hdf5/lib:${LD_LIBRARY_PATH} -np 64 --hostfile hosts1 --map-by node ${dir}/venv_par/bin/python ${dir}/hisscube.py "$@"
+mpirun -x DARSHAN_CONFIG_PATH=${dir}/darshan.conf -x LD_PRELOAD=${dir}/../shared_libs/darshan/darshan-runtime/lib/.libs/libdarshan.so:${dir}/ext_lib/hdf5-1.12.2/hdf5/lib/libhdf5.so -x LD_LIBRARY_PATH=${dir}/ext_lib/hdf5-1.12.2/hdf5/lib:${LD_LIBRARY_PATH} -np "$1" --hostfile hosts --map-by node ${dir}/venv_par/bin/python ${dir}/hisscube.py "${@:2}"
